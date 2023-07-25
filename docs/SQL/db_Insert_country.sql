61672d83acf0e9aca9c8a3cf336f94913e5fbe3b
unknown <masa.x.mo@gmail.com>
2023-07-24 21:00:01 -0400

    Added DB docs.

---
+++ docs/SQL/db_Insert_country.sql
@@ -0,0 +1,24 @@
+-- country ---
+DELETE from `dm_changemaker_sb`.`country`;
+
+INSERT INTO `dm_changemaker_sb`.`country`
+(`country_code`,`country_name`)
+VALUES
+('AD','Andorra');
+
+INSERT INTO `dm_changemaker_sb`.`country`
+(`country_code`,`country_name`)
+VALUES
+('AE','United Arab Emirates');
+
+INSERT INTO `dm_changemaker_sb`.`country`
+(`country_code`,`country_name`)
+VALUES
+('CA','Canada');
+
+INSERT INTO `dm_changemaker_sb`.`country`
+(`country_code`,`country_name`)
+VALUES
+('US','United States of America');
+
+-- SELECT * from `dm_changemaker_sb`.`country`;
