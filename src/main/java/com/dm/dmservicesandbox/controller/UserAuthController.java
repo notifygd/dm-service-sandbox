5444e6361f7612502101f1d506e525b96f8a41c6
gargs <gargshobhit07@gmail.com>
2023-08-10 19:46:12 -0400

    User Registration V1

--- src/main/java/com/dm/dmservicesandbox/controller/UserAuthController.java
+++ src/main/java/com/dm/dmservicesandbox/controller/UserAuthController.java
@@ -9,10 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 
 @Slf4j
@@ -31,5 +29,10 @@ public class UserAuthController {
             return new ResponseEntity<>(new ServiceReponse(false, "Email Address already in use!"), HttpStatus.BAD_REQUEST);
         }
         return ResponseEntity.ok().body(new ServiceReponse(true, "User registration success"));
+    }
+
+    @GetMapping("ping")
+    public String ping() {
+        return String.format("The app is running. Ping Successful");
     }
 }
